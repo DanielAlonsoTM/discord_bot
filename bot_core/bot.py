5a600cae3cca84feba6882f1895379fbc74383a7
Daniel Tapia <daniel.tapia@klap.cl>
2023-03-28 11:57:16 -0300

    Refactor chat messages

--- bot_core/bot.py
+++ bot_core/bot.py
@@ -1,22 +1,21 @@
 import discord
 from discord.ext import commands
-import reply_handlers.responses as responses
 from jproperties import Properties
-from utils.url_check import url_check
 from bot_core.commands.music import commands_music
+from bot_core.commands.chat import commands_chat
 
 
-async def send_message(message: str, user_message: str, is_private: bool, is_custom: bool = None, motive: str = None):
-    try:
-        if is_custom:
-            response = responses.handle_custom_message(motive)
-            await message.channel.send(response)
-        else:
-            response = responses.handle_message(user_message)
-            await message.author.send(response) if is_private else await message.channel.send(response)
+# async def send_message(message: str, user_message: str, is_private: bool, is_custom: bool = None, motive: str = None):
+#     try:
+#         if is_custom:
+#             response = responses.handle_custom_message(motive)
+#             await message.channel.send(response)
+#         else:
+#             response = responses.handle_message(user_message)
+#             await message.author.send(response) if is_private else await message.channel.send(response)
 
-    except Exception as e:
-        print(f'Exception: {e}')
+#     except Exception as e:
+#         print(f'Exception: {e}')
 
 
 def run_discord_bot():
@@ -31,42 +30,11 @@ def run_discord_bot():
     intents = discord.Intents.all()
     bot = commands.Bot(command_prefix='.', intents=intents)
 
-    @bot.event
-    async def on_ready():
-        print(f'Boot {bot.user} is now running')
+    # @bot.event
+    # async def on_ready():
+    #     print(f'Boot {bot.user} is now running')
 
-    @bot.event
-    async def on_message(message):
-        # bot.process_commands(msg) is a couroutine that must be called here since we are overriding the on_message event
-        await bot.process_commands(message)
-        
-        # Make sure bot doesn't get stuck in an infinite loop       
-        if message.author == bot.user:
-            return
-        
-        # Get data about the user
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
-
-        # Debug printing
-        print(
-            f"================\n{username} said: '{user_message}' \nChannel: ({channel})\n================\n")
-        
-        if len(user_message) > 0:
-            # Check if message is an url
-            if url_check(user_message):
-                await send_message(message, user_message, is_private=False, is_custom=True, motive='url')
-            else:
-                # If the user message contains a '?' in front of the text, it becomes a private message
-                if user_message[0] == '?':
-                    user_message = user_message[1:]  # [1:] Removes the '?'
-                    await send_message(message, user_message, is_private=True)
-                else:
-                    await send_message(message, user_message, is_private=False)
-        else:
-            await send_message(message, user_message, is_private=False)
-
+    commands_chat(bot)
     commands_music(bot)
 
     bot.run(token_key)
